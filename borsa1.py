3bb881c5af69b39ab8d47408991474b0d4276d70
suayipc <suayipc@gmail.com>
2023-04-01 07:54:12 +0300

    Update borsa1.py

--- borsa1.py
+++ borsa1.py
@@ -1,8 +1,6 @@
 import streamlit as st
-pip install matplotlib
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
 import yfinance as yf # https://pypi.org/project/yfinance/
 from ta.volatility import BollingerBands
